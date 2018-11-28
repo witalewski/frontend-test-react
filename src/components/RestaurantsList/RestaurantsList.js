108427823b36cf0161c6a3b1171e29cc88c860c9
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-11-28 16:39:11 +0100

    add restaurants list component

---
+++ src/components/RestaurantsList/RestaurantsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RestaurantsList extends Component {
+
+    render() {
+        return <div>[Restaurants list]</div>
+    }
+}
+
+export { RestaurantsList };
+export default RestaurantsList;
