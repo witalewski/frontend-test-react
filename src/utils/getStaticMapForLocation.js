606bcd6ac59e031f533028f443fd7af60f5cdc75
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-11-29 01:59:03 +0100

    adjust map zoom

--- src/utils/getStaticMapForLocation.js
+++ src/utils/getStaticMapForLocation.js
@@ -1,4 +1,4 @@
 export const getStaticMapForLocation = ({ latitude, longitude }) => {
   const coordsAsText = [latitude, longitude].join(',');
-  return `https://maps.googleapis.com/maps/api/staticmap?center=${coordsAsText}&markers=${coordsAsText}&zoom=14&size=400x288&maptype=roadmap&key=AIzaSyCDc4LkExUhXGwButHuEENHOTE3iAJ8k2E`;
+  return `https://maps.googleapis.com/maps/api/staticmap?center=${coordsAsText}&markers=${coordsAsText}&zoom=15&size=400x288&maptype=roadmap&key=AIzaSyCDc4LkExUhXGwButHuEENHOTE3iAJ8k2E`;
 };
